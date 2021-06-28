db80dc17dab1280df3143231b591cb34787d062c
Daniel-Alencar <danielalencar746@gmail.com>
2021-06-28 12:16:10 -0300

    feat: validation added for user creation

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,9 +2,24 @@ import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepositories';
 
+import * as yup from 'yup';
+
 class UserController {
   async create(request: Request, response: Response) {
     const { email, name } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch(error) {
+      return response.status(400).json({
+        error: error 
+      });
+    }
 
     const usersRepository = getCustomRepository(UsersRepository);
     // SELECT * FROM USERS WHERE EMAIL = "EMAIL"
